fe92a268c764da25faacad88fd14eae36be3d8d2
ldl <1900018250@qq.com>
2020-01-28 08:50:35 +0800

    9

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 const hello = require('./hello');
 const world = require('./world');
+import a from 'lib/say'
+import(
+  /*
+  webpackChunkName: 'layzz-name'
+  */
+  './layzz'
+).then(layzz => void(console.log(layzz)))
+// console.log(layzz)
 console.log(`${hello} ${world}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path')
-module.exports = {
-  mode: 'development',
-  entry: './foo.js',
-  output: {
-  path: path.resolve(__dirname, 'dist'),
-  filename: 'foo.bundle.js'
- }
+
+module.exports = (env, argv) => {
+  // console.log(env)
+return {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'foo.bundle.js'
+    },
+    resolve: {
+      extensions: ['.js', '.json', '.css'],
+      alias: {
+        'lib': path.resolve(__dirname, 'src/lib')
+      },
+    }
  };
+}
